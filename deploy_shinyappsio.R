c6295cdb7fc2f8e008bd18228b84f07c7a9174cc
alexkarvou <karvouniaris.alexander@gmail.com>
2018-05-12 16:27:32 +0300

    deploying apps in shinyapps.io

---
+++ deploy_shinyappsio.R
@@ -0,0 +1,5 @@
+library(rsconnect)
+rsconnect::setAccountInfo(name='kernel-ai',
+                          token='44FB6031E17201E48C0DBB3E1CB0986B',
+                          secret='gvNKk1zRj9KBtRRCB88yZTBirAxF8BsvY+eDBDDM')
+rsconnect::deployApp('C:/Users/karvo/Desktop/vinocular/crack-beginning/shiny_app/kmeans')
